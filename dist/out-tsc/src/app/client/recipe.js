8b89d39f8f05c94a5be967939f90737eb5733076
Joshua Lewis <joshilewis@gmail.com>
2017-12-21 16:46:50 +0200

    removes dist folder

--- dist/out-tsc/src/app/client/recipe.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Recipe = /** @class */ (function () {
-    function Recipe() {
-    }
-    return Recipe;
-}());
-exports.Recipe = Recipe;
-//# sourceMappingURL=recipe.js.map
